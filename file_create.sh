da072340d20f2c701b2231f5e004fdbf2abe7a9b
tier <tier1985@gmail.com>
2023-10-05 13:53:10 +0200

    added files

--- file_create.sh
+++ file_create.sh
@@ -2,10 +2,17 @@
 
 # Создание файлов на основе списка уроков
 declare -a lessons=(
-"1_lesson_Basic_variables"
-"2_lesson_Type_conversion_and_input"
-"3_lesson_Data_operations"
-"4_lesson_Conditional_statements"
+#"1_lesson_Basic_variables"
+#"2_lesson_Type_conversion_and_input"
+#"3_lesson_Data_operations"
+#"4_lesson_Conditional_statements"
+"5_lesson_Arrays"
+"6_lesson_Loops"
+"7_lesson_Lists_and_Tuples"
+"8_lesson_Functions"
+"9_lesson_Dictionaries"
+"10_lesson_Strings"
+"11_lesson_Basics_of_OOP"
 )
 
 # Цикл для создания файлов
